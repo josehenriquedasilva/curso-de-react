228f659a40c91af972d720508c67b964b0e11cb4
josehenriquedasilva <josehenrique60t@gmail.com>
2025-04-10 16:13:03 -0300

    correcao

--- src/componentes/AddTarefas.jsx
+++ src/componentes/AddTarefas.jsx
@@ -9,7 +9,7 @@ function AddTarefas({tarefaAdicionada}) {
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
         <Input 
             type="text" 
-            placeholder="Digite o título aqui"
+            placeholder="Digite o título"
             value={titulo}
             onChange={(event) => setTitulo(event.target.value)}
         />
