c473fd02e3aa1ed072a43f6d1b6de0c7b5ddc45a
josehenriquedasilva <josehenrique60t@gmail.com>
2025-04-10 14:58:23 -0300

    correcao

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import PaginaTarefas from './paginas/paginaTarefas.jsx';
+import PaginaTarefas from './paginas/PaginaTarefas.jsx';
 
 const router = createBrowserRouter([
   {
