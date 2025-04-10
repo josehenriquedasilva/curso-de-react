0282de7b0651f79d2925657976e96fa95aefc0bd
josehenriquedasilva <josehenrique60t@gmail.com>
2025-04-10 16:47:30 -0300

    correcao

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,8 @@ function App() {
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
         <Titulo>Gerenciador de Tarefas</Titulo>
-        <AddTarefas tarefaAdicionada={tarefaAdicionada} />
+        <AddTarefas 
+        tarefaAdicionada={tarefaAdicionada} />
         <Tarefas
           tarefas={tarefas}
           tarefaClicada={tarefaClicada}
